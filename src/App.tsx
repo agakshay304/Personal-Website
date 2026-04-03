4d2fdd319ae7a61d62a3e45168ec66570c36a353
Akshay Gupta <agakshay304@gmail.com>
2026-04-03 09:31:00 +0530

    compose the single-page portfolio flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import AppLayout from "./components/layout/AppLayout";
+import AboutSection from "./components/sections/AboutSection";
+import ContactSection from "./components/sections/ContactSection";
+import ExperienceSection from "./components/sections/ExperienceSection";
+import ExpertiseSection from "./components/sections/ExpertiseSection";
+import HeroSection from "./components/sections/HeroSection";
+import ProjectsSection from "./components/sections/ProjectsSection";
+import TechStackSection from "./components/sections/TechStackSection";
+
 const App = () => {
   return (
-    <div className="app-shell">
-      <p>Initializing portfolio workspace.</p>
-    </div>
+    <AppLayout>
+      <HeroSection />
+      <AboutSection />
+      <ExpertiseSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <TechStackSection />
+      <ContactSection />
+    </AppLayout>
   );
 };
 
